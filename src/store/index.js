2d4c641abd17859b1aa2409e166062bacc47ea0f
hellelio <freadwantblossom@gmail.com>
2021-11-30 00:27:35 +0300

    add loading state and described the logic for it

--- src/store/index.js
+++ src/store/index.js
@@ -7,15 +7,20 @@ export default createStore({
     inputValue: '',
     errorFetch: false,
     searchState: false,
+    loading: false,
     weather: {},
   },
   getters: {
     errorFetch: (state) => state.errorFetch,
     searchState: (state) => state.searchState,
     weather: (state) => state.weather,
+    loading: (state) => state.loading,
   },
   methods: {},
   mutations: {
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
     setWeather(state, payload) {
       state.weather = payload;
     },
@@ -32,19 +37,24 @@ export default createStore({
   actions: {
     async fetchWeather(state, payload) {
       this.commit('setInputState', payload);
-      await fetch(
-        `${this.state.base}weather?q=${this.state.inputValue}&units=metric&APPID=${this.state.apiKey}`,
-      )
-        .then((res) => res.json())
-        .then(async (result) => {
-          if (result.cod === '404') {
-            this.commit('setErrorFetch', true);
-          } else {
-            this.commit('setWeather', result);
-            this.commit('setSearchState', true);
-            this.commit('setErrorFetch', false);
-          }
-        });
+      this.commit('setSearchState', true);
+      this.commit('setLoading', true);
+      setTimeout(() => {
+        fetch(
+          `${this.state.base}weather?q=${this.state.inputValue}&units=metric&APPID=${this.state.apiKey}`,
+        )
+          .then((res) => res.json())
+          .then((result) => {
+            if (result.cod === '404') {
+              this.commit('setErrorFetch', true);
+            } else {
+              this.commit('setLoading', false);
+              this.commit('setWeather', result);
+              this.commit('setSearchState', true);
+              this.commit('setErrorFetch', false);
+            }
+          });
+      }, 1000);
     },
   },
   modules: {},
