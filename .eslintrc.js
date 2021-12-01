20e98cf0e9e887664b00241adb1a995330f20e3e
helloelio <fredwantblossom@gmail.com>
2021-12-01 16:43:09 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/prettier', 'eslint:recommended'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   parserOptions: {
     parser: 'babel-eslint',
   },
