bf8cf8fea99def98b5d578f47a246448bbbdfc72
helloelio <fredwantblossom@gmail.com>
2021-12-02 13:57:23 +0300

    change font family

--- src/App.vue
+++ src/App.vue
@@ -58,10 +58,10 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@100;300;400;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700&display=swap');
 #app {
   background-position: center;
-  font-family: 'M PLUS 1 Code', sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
